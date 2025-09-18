c971a3e143e43875b59f032ed6f1054001800c85
Ryan <ryancentralorg@gmail.com>
2025-09-18 01:07:53 +0000

    eta LoRA loader

--- diffsynth/models/utils.py
+++ diffsynth/models/utils.py
@@ -72,9 +72,10 @@ def load_state_dict(file_path, torch_dtype=None, device="cpu"):
 def load_state_dict_from_safetensors(file_path, torch_dtype=None, device="cpu"):
     state_dict = {}
     with safe_open(file_path, framework="pt", device=str(device)) as f:
-        for k in f.keys():
+        import rp
+        for k in rp.eta(f.keys(), f'LoRA LOADING'):
             state_dict[k] = f.get_tensor(k)
-            print("LOADED",k)
+            # print("LOADED",k)
             if torch_dtype is not None:
                 state_dict[k] = state_dict[k].to(torch_dtype)
     return state_dict
